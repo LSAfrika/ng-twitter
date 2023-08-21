9e4fdef39410d6451331feb01c913b662bb1cec6
LSAfrika <lstudiosafrika@gmail.com>
2023-08-21 16:02:54 +0300

    began work on home page ui

--- src/app/shared/shared/shared.module.ts
+++ src/app/shared/shared/shared.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavComponent } from '../../components/nav/nav.component';
+import { TweetComponent } from '../../components/tweet/tweet.component';
+import { SidenavComponent } from '../../components/sidenav/sidenav.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavComponent,
+    TweetComponent,
+    SidenavComponent
+  ],
   imports: [
     CommonModule
+  ],exports:[
+    NavComponent,
+    TweetComponent
   ]
 })
 export class SharedModule { }
