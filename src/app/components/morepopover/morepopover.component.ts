a6a6a16862fc48c8ad0b596349d027eb5296e5bb
LSAfrika <lstudiosafrika@gmail.com>
2023-08-29 08:52:19 +0300

    90% done with side nav component

--- src/app/components/morepopover/morepopover.component.ts
+++ src/app/components/morepopover/morepopover.component.ts
@@ -8,4 +8,35 @@ import { UiService } from 'src/app/service/ui.service';
 })
 export class MorepopoverComponent {
 constructor(public UIS:UiService){}
+
+
+ngOnDestroy(){
+
+}
+
+togglecreator(){
+  this.UIS.dropdowncreator$.value?this.UIS.dropdowncreator$.next(false):this.UIS.dropdowncreator$.next(true)
+
+  console.log('drop down creatore value',this.UIS.dropdowncreator$.value);
+
+}
+
+
+
+toggleprofessional(){
+  this.UIS.dropdownprofessional$.value?this.UIS.dropdownprofessional$.next(false):this.UIS.dropdownprofessional$.next(true)
+
+  // console.log('drop down creatore value',this.UIS.dropdowncreator$.value);
+
+}
+
+togglesettings(){
+  this.UIS.dropdownsettings$.value?this.UIS.dropdownsettings$.next(false):this.UIS.dropdownsettings$.next(true)
+
+  // console.log('drop down creatore value',this.UIS.dropdowncreator$.value);
+
+}
+
+
+
 }
--- src/app/service/ui.service.ts
+++ src/app/service/ui.service.ts
@@ -10,12 +10,13 @@ export class UiService {
 
 selectednavigation=0
 
+disabled=true
 public popover$=new BehaviorSubject(false)
 public dropdowncreator$=new BehaviorSubject(false)
 public dropdownprofessional$=new BehaviorSubject(false)
 public dropdownsettings$=new BehaviorSubject(false)
 constructor(private router:Router){}
-
+public popoverobs$=this.popover$.asObservable()
 switchnav(input:number,route:string){
   this.selectednavigation=input
   this.router.navigateByUrl(`${route}`)
@@ -23,11 +24,33 @@ switchnav(input:number,route:string){
 }
 
 disablepopover(){
+
+  if(this.disabled==true)return
   this.popover$.next(false)
+    this.dropdowncreator$.next(false)
+  this.dropdownprofessional$.next(false)
+  this.dropdownsettings$.next(false)
+  console.log('pop over disabled');
+  this.disabled=true
+
 }
 
 enablepopover(){
   this.popover$.next(true)
+
+}
+
+leave(){
+
+  this.disabled=true
+
+  // console.log('mouse left');
+
+}
+
+enter(){
+  this.disabled=false
+  // console.log('mouse enter');
 
 }
 
